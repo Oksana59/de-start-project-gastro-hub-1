5e7545c45e5ef20a77baf73c646137708c332dcc
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 12:01:32 +0300

    Update INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,1 +1,35 @@
-/*Добавьте в этот файл запросы, которые наполняют данными таблицы в схеме cafe данными*/
+INSERT INTO cafe.restaurants (cafe_name, type, menu)
+SELECT DISTINCT 
+s.cafe_name, 
+CAST(s.type AS cafe.restaurant_type), 
+m.menu
+FROM raw_data.sales s
+LEFT JOIN raw_data.menu m ON s.cafe_name = m.cafe_name 
+;
+
+INSERT INTO cafe.managers (manager, manager_phone)
+SELECT DISTINCT manager, manager_phone 
+FROM raw_data.sales
+;
+
+INSERT INTO cafe.restaurant_manager_work_dates (restaurant_uuid, manager_uuid, date_work_begin, date_work_end)
+SELECT DISTINCT
+r.restaurant_uuid,
+m.manager_uuid, 
+MIN(s.report_date),
+MAX(s.report_date)
+FROM raw_data.sales s 
+LEFT JOIN cafe.managers m ON s.manager = m.manager 
+LEFT JOIN cafe.restaurants r ON s.cafe_name = r.cafe_name 
+GROUP BY r.restaurant_uuid, m.manager_uuid
+;
+
+INSERT INTO cafe.sales (date, restaurant_uuid, avg_check)
+SELECT 
+s.report_date,
+r.restaurant_uuid,
+s.avg_check 
+FROM raw_data.sales s
+LEFT JOIN cafe.restaurants r ON s.cafe_name = r.cafe_name
+;
+
