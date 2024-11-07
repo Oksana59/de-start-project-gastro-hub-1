d2533e1128f61c0754df5e713ece95af3f14ac78
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 11:59:32 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,1 +1,22 @@
-/*добавьте сюда запросы для решения задания 6*/
+BEGIN;
+
+WITH pizza AS (
+SELECT 
+cafe_name,
+'"' || new_price::TEXT || '"' AS price_cappuccino
+FROM(
+	SELECT 
+	cafe_name,
+	ROUND((menu -> 'Кофе' -> 'Капучино')::INTEGER *1.2) AS new_price
+	FROM cafe.restaurants r  
+	WHERE menu -> 'Кофе' ? 'Капучино'
+ 	FOR NO KEY UPDATE
+	) a
+	)
+UPDATE cafe.restaurants r
+SET menu = JSONB_SET(menu, '{Кофе, Капучино}', p.price_cappuccino::JSONB)
+FROM pizza p
+WHERE p.cafe_name = r.cafe_name;
+
+COMMIT;
+
