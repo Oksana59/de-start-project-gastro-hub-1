577ed7e84181b1ff9ed5fafbf4210e48f7e26f11
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 11:58:46 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,25 @@
-/*добавьте сюда запрос для решения задания 5*/
+WITH pizza AS (
+	SELECT 
+	cafe_name,
+	menu -> 'Пицца' AS product
+	FROM cafe.restaurants r  
+	WHERE type = 'pizzeria'
+	)
+SELECT 
+cafe_name,
+pizza_type,
+key AS name_pizza,
+value AS price
+FROM (
+	SELECT a.*,
+	ROW_NUMBER() OVER (PARTITION BY cafe_name ORDER BY value DESC) AS rn
+	FROM (
+		SELECT cafe_name,
+		'Пицца' AS pizza_type,
+		(JSONB_EACH(product)).key,
+		(JSONB_EACH(product)).value
+		FROM pizza
+		) a
+	) b
+WHERE rn =1
+;
