a40a7a98fd85ab494fb840b841493608bb355b1a
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 11:57:44 +0300

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,15 @@
-/*добавьте сюда запрос для решения задания 3*/
+SELECT 
+cafe_name, COUNT(manager_uuid)
+FROM (
+SELECT DISTINCT 
+r.cafe_name, 
+rm.manager_uuid,
+rm.date_work_begin,
+rm.date_work_end
+FROM cafe.restaurant_manager_work_dates rm
+LEFT JOIN cafe.restaurants r ON r.restaurant_uuid = rm.restaurant_uuid 
+) a
+GROUP BY cafe_name
+ORDER BY COUNT(manager_uuid) DESC
+LIMIT 3
+;
