6e24fd082fb916df008d447787942cc890e2dfe2
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 11:56:51 +0300

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,1 +1,15 @@
-/*добавьте сюда запрос для решения задания 2*/
+CREATE MATERIALIZED VIEW avg_bill_change AS 
+SELECT a.*,
+LAG(bill_avg_current_year, 1) OVER (PARTITION BY cafe_name) AS bill_avg_previous_year,
+ROUND(bill_avg_current_year *100 / (LAG(bill_avg_current_year, 1) OVER (PARTITION BY cafe_name)) -100, 2) AS delta_percent
+FROM (
+	SELECT 
+	EXTRACT(YEAR FROM s.date) AS YEAR, r.cafe_name, r.type, 
+	ROUND(AVG(avg_check), 2) AS bill_avg_current_year
+	FROM cafe.sales s
+	LEFT JOIN cafe.restaurants r ON s.restaurant_uuid = r.restaurant_uuid 
+	WHERE EXTRACT(YEAR FROM s.date) != 2023
+	GROUP BY year, r.cafe_name, r.type
+	ORDER BY r.cafe_name, year
+	) a
+;
