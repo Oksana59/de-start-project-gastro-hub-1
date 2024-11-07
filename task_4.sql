2ba76c7b543a3f27fe9402603a2a52245bf5edf7
Oksana59 <oksana-pospelova@mail.ru>
2024-11-07 11:58:16 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,24 @@
-/*добавьте сюда запрос для решения задания 4*/
+WITH pizza AS (
+	SELECT 
+	cafe_name,
+	menu -> 'Пицца' AS product
+	FROM cafe.restaurants r  
+	WHERE type = 'pizzeria'
+	)
+SELECT cafe_name, count_pizza 
+FROM (
+	SELECT b.*,
+	DENSE_RANK() OVER (ORDER BY count_pizza DESC) AS rn
+	FROM (
+		SELECT cafe_name, COUNT(*) AS count_pizza
+		FROM (
+			SELECT cafe_name,
+			(JSONB_EACH(product)).KEY
+			FROM pizza
+			) a
+		GROUP BY cafe_name
+		ORDER BY COUNT(*) DESC
+		) b
+) c
+WHERE rn =1
+;
